128e131a0aed3918ef6d276b39f07460be5e04fc
Bersaaker <alex.aven.luna@gmail.com>
2024-02-21 16:12:13 -0600

    rutas

---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,12 @@
+interface CategoryProps {
+    params: {
+        category: string
+    }
+}
+
+export default function category(props: CategoryProps) {
+    const { category } = props.params
+    return (
+        <h1>Categoria dinámica {category}</h1>
+    )
+}
